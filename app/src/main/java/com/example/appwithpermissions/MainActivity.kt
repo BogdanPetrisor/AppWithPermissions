94562ff490fc8023879438c3d1f2dab3be28c5e4
Petrisor Bogdan <bogdan.petrisor@yopeso.com>
2022-07-19 11:29:51 +0300

    Create 3 functions to check if the permission is granted

--- app/src/main/java/com/example/appwithpermissions/MainActivity.kt
+++ app/src/main/java/com/example/appwithpermissions/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.appwithpermissions
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun hasWriteExternalStoragePermission() =
+        ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasForegroundLocationPermission() =
+        ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasBackgroundLocationPermission() =
+        ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
 }
